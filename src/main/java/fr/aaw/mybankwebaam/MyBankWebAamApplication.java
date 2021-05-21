543bc68615f323e5eb8524ce5ded8d7b574d3a15
mikos974 <michael974.parmentier@gmail.com>
2021-05-21 16:08:34 +0200

    Initial commit

---
+++ src/main/java/fr/aaw/mybankwebaam/MyBankWebAamApplication.java
@@ -0,0 +1,13 @@
+package fr.aaw.mybankwebaam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankWebAamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBankWebAamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/aaw/mybankwebaam/MyBankWebAamApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.aaw.mybankwebaam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankWebAamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
