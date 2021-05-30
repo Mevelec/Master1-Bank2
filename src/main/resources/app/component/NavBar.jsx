bb6e7105188f6effde56eb463077e90c528ca02e
mevelec <adrien.mevelec@hotmail.fr>
2021-05-30 19:39:35 +0200

    update index

--- src/main/resources/app/component/NavBar.jsx
+++ src/main/resources/app/component/NavBar.jsx
@@ -9,19 +9,9 @@ function NavBar(){
             <ReactBootstrap.Navbar.Collapse id="basic-navbar-nav">
                 <ReactBootstrap.Nav className="mr-auto">
                 <ReactBootstrap.Nav.Link href="#home">Home</ReactBootstrap.Nav.Link>
-                <ReactBootstrap.NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <ReactBootstrap.NavDropdown.Item href="#action/3.1">Action</ReactBootstrap.NavDropdown.Item>
-                    <ReactBootstrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootstrap.NavDropdown.Item>
-                    <ReactBootstrap.NavDropdown.Item href="#action/3.3">Something</ReactBootstrap.NavDropdown.Item>
-                    <ReactBootstrap.NavDropdown.Divider />
-                    <ReactBootstrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootstrap.NavDropdown.Item>
-                </ReactBootstrap.NavDropdown>
                 </ReactBootstrap.Nav>
             </ReactBootstrap.Navbar.Collapse>
         </ReactBootstrap.Navbar>
-
-
-        
     );
 }
 export default NavBar;
