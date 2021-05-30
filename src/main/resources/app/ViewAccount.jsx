e941b09c0e88899495d167dbee2f1fc173ee4e6b
Mikos974 <michael974.parmentier@gmail.com>
2021-05-30 23:46:29 +0200

    ajout du fichier appelant ViewAccount.jsx

---
+++ src/main/resources/app/ViewAccount.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ViewAccount from "./component/ViewAccount";
+
+ReactDOM.render(
+    <ViewAccount/>,    document.getElementById('main'));
